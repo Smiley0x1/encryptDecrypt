0e56cc8ca5ef57ad0c3b77aeff4aacacefd335f7
Logan <58321687+Smyli3@users.noreply.github.com>
2022-07-03 18:54:37 -0700

    New encryption

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from unicodedata import name
 import reverse
 import ceaser
+import transposition
 
 
 if __name__=="__main__":
-    ques = int(input("What would you like to do?\n 1) Reverse String Encryption \n 2) Ceaser Cypher\n"))
+    ques = int(input("What would you like to do?\n 1) Reverse String Encryption \n 2) Ceaser Cypher\n 3) Transposition cypher \n"))
     if ques == 1:
         reverse.run()
-    if ques ==2:
+    if ques == 2:
         ceaser.run()
+    if ques == 3:
+        ransposition.run()
---
+++ transposition.py
@@ -0,0 +1,4 @@
+
+
+def run():
+    print("This is a work in progress stay tuned")
